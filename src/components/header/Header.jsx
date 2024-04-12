126208a1e94213d515e3272355a44b7b77013660
Aleksi Svaridze <34221930+aleksi-svaridze@users.noreply.github.com>
2024-04-12 08:51:39 +0400

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     const [theme, setTheme] = useState(
         localStorage.getItem('theme') ? localStorage.getItem('theme') : 'system'
     );
-    const [toggleMenu, setToggleMenu] = useState(true);
+    const [toggleMenu, setToggleMenu] = useState(false);
 
     const element = document.documentElement;
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
@@ -115,7 +115,7 @@ const Header = () => {
             </div>
             <div 
                 style={{height: 'calc(100vh - 66px)'}} 
-                className={`items-center pt-10 flex flex-col gap-y-5 md:hidden w-3/4 md:w-auto sm:w-1/2 bg-slate-500 mt-[66px] duration-500 ${toggleMenu ? '-right-full' : 'right-0'}`}
+                className={`items-center pt-10 flex flex-col gap-y-5 md:hidden md:bg-transparent w-3/4 md:w-auto sm:w-1/2 bg-slate-500 mt-[66px] duration-500 ${toggleMenu ? '-right-full' : 'right-0'}`}
             >
                 <NavLink 
                     className={({isActive}) => `${isActive && 'text-red-500 dark:text-yellow-500'} text-blue-500 dark:text-white font-opensans font-semibold text-base mx-5 tracking-wide`} 
