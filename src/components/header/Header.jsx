54658200a7aa794046f60c6f406a406559d1da5f
Aleksi Svaridze <34221930+aleksi-svaridze@users.noreply.github.com>
2024-04-12 15:02:20 +0400

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -109,13 +109,13 @@ const Header = () => {
                 </nav>
                 <div 
                     onClick={() => setToggleMenu(!toggleMenu)} 
-                    className="md:hidden cursor-pointer text-white w-12 h-12">
-                        <ion-icon name="menu-outline"></ion-icon>
+                    className="md:hidden cursor-pointer text-white">
+                        <ion-icon className="w-8 h-8" name="menu-outline"></ion-icon>
                 </div>
             </div>
             <div 
                 style={{height: 'calc(100vh - 66px)'}} 
-                className={`items-center pt-10 flex flex-col gap-y-5 md:hidden w-3/4 md:w-auto sm:w-1/2 bg-slate-500 md:bg-transparent top-[66px] bottom-0 duration-500 absolute ${toggleMenu ? 'right-0' : '-right-full'}`}
+                className={`items-center pt-10 flex flex-col gap-y-5 md:hidden w-3/4 md:w-auto sm:w-1/2 bg-slate-500 md:bg-transparent top-[66px] bottom-0 duration-500 relative ${toggleMenu ? 'right-0' : '-right-full'}`}
             >
                 <NavLink 
                     className={({isActive}) => `${isActive && 'text-red-500 dark:text-yellow-500'} text-blue-500 dark:text-white font-opensans font-semibold text-base mx-5 tracking-wide`} 
