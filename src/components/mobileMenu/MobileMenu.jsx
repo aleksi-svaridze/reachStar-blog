c9f07294e6dd69d9f3e6591a749498b6d04963cb
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-04-28 12:43:57 +0400

    Fix: mobile links login && logout sequence

--- src/components/mobileMenu/MobileMenu.jsx
+++ src/components/mobileMenu/MobileMenu.jsx
@@ -3,44 +3,53 @@ import scrollToTop from '../../functions/scrolToTop'
 
 
 export default function MobileMenu({isOpen, isLoggedIn, setIsLoggedIn}) {
+
   return (
     <div 
-    className={`items-center pt-10 flex flex-col gap-y-5 md:hidden w-2/3 bg-blue-100 border-l border-t md:bg-transparent duration-500
+        className={`items-center justify-center flex flex-col gap-y-5 md:hidden w-2/3 bg-blue-100 border-l border-t md:bg-transparent duration-500
      ${isOpen ? 'right-0' : '-right-full'} lg:static fixed top-[66px] bottom-0`}
 >
-    <NavLink 
-        className={`text-blue-500 font-opensans font-semibold text-base mx-5 tracking-wide`} 
-        onClick={scrollToTop} to={'/'}
-    >
-        Home
-    </NavLink> 
-    <NavLink 
-        className={`text-blue-500 font-opensans font-semibold text-base mx-5 tracking-wide`}  
-        onClick={scrollToTop} to={'/blog'}
-    >
-        Blog
-    </NavLink>
     {
         isLoggedIn && (
+        <>
+            <NavLink 
+                className={`text-blue-500 font-opensans font-semibold text-base mx-5 tracking-wide`} 
+                onClick={scrollToTop} to={'/'}
+            >
+                Home
+            </NavLink> 
+            <NavLink 
+                className={`text-blue-500 font-opensans font-semibold text-base mx-5 tracking-wide`}  
+                onClick={scrollToTop} to={'/blog'}
+            >
+                Blog
+            </NavLink>
             <NavLink 
                 className={`text-blue-500 font-opensans font-semibold text-base mx-5 tracking-wide`}  
                 onClick={scrollToTop} to={'/dashboard'}
             >
                 dashboard
             </NavLink>
+        </>
         )
     }
-    <NavLink 
-        onClick={() => {
-            setIsLoggedIn(false)
-        }}
-        className={`text-blue-500 font-opensans font-semibold text-base text-center border-2 py-2 w-[100px] rounded-full border-blue-500 tracking-wide`}  
-        to={`${isLoggedIn && '/login'}`}
-    >
-        {
-            isLoggedIn ? 'Log Out' : 'Log In' 
-        }
-    </NavLink>
+    {
+        isLoggedIn ?  
+        <NavLink 
+            onClick={() => setIsLoggedIn(false)}
+            className={`text-blue-500 font-opensans font-semibold text-base text-center border-2 py-2 w-[100px] rounded-full border-blue-500 tracking-wide`}  
+            to={'/login'}
+        >
+            Log Out
+        </NavLink>
+        :
+        <NavLink 
+            className={`text-blue-500 font-opensans font-semibold text-base text-center border-2 py-2 w-[100px] rounded-full border-blue-500 tracking-wide`}  
+            to={'/login'}
+        >
+            Log In
+        </NavLink>
+    }
     {
         isLoggedIn ? '' : (<NavLink 
             className={`text-blue-500 font-opensans font-semibold text-base text-center border-2 py-2 w-[100px] rounded-full border-blue-500 tracking-wide`}  
